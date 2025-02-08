3dff511ce55ed33fdfb5e2de6bb84d7404d49b47
GildedFlames <GildedFlames@github.com>
2025-02-08 16:51:28 +0800

    Initial release v1.0.0: Support multi-account sign-in for Genshin and StarRail.

---
+++ README.md
@@ -0,0 +1,114 @@
+# 米家游戏米游社签到脚本 (Node.js 版) - 支持多账号 (崩铁 & 原神)
+
+## 简介
+本项目是一个用于自动签到米家游戏（崩铁、原神）米游社的 Node.js 脚本。支持多账号，并可本地部署或通过 GitHub Actions 自动运行。
+
+## 功能特性
+- **多账号支持**: 同时支持崩铁和原神多个账号的签到。
+- **本地部署**: 可以在本地或服务器上运行脚本。
+- **GitHub Actions 部署**: 可以通过 GitHub Actions 定期自动运行签到任务。
+
+## 免责声明
+本脚本仅供交流测试使用。因使用本脚本而产生的任何问题，作者概不负责。官方可能更改接口导致脚本失效，脚本失效会尽快更新，但不保证第一时间。请低调使用，如有不同意，请关闭并停止使用。
+
+## 获取 Cookie 方式 - 必需
+
+1. **打开浏览器**:
+   - 打开你的浏览器，进入无痕/隐身模式。
+   
+2. **登录米游社**:
+   - 访问 [原神论坛](http://bbs.mihoyo.com/ys) 或 [崩铁论坛](http://bbs.mihoyo.com/sr)，二选一进行登录操作。
+   
+3. **获取 Cookie**:
+   - 按下键盘上的 `F12` 或右键点击页面选择“检查”，打开开发者工具。
+   - 切换到“Console”选项卡，复制粘贴以下代码：
+     ```js
+     const cookie = document.cookie
+      const ask = confirm('Cookie:' + cookie + '\n\nDo you want to copy the cookie to the clipboard?')
+      if (ask == true) {
+        copy(cookie)
+        msg = cookie
+      } else {
+        msg = 'Cancel'
+      }
+     ```
+   - 按下回车键，此时 Cookie 已经复制到你的剪贴板，原神崩铁Cookie通用。
+
+
+
+## GitHub Actions 部署
+
+你可以通过 GitHub Actions 定期自动运行签到任务。以下是配置步骤：
+
+1. **Fork本仓库**
+
+2. **配置 GitHub Secrets**:
+   - 打开你的 Fork 仓库页面。
+   - 点击 `Settings` -> `Secrets and variables` -> `Actions`。
+   - 添加新的 Secret, 多个cookie用英文逗号(,)分隔，若cookie下有原神/崩铁角色会执行签到：
+     - 名称: `GENSHIN_COOKIES`
+       值: `cookie1,cookie2,cookie3`
+
+3. **创建 Workflow 文件**:
+   - 在 `.github/workflows` 目录下创建一个新的 YAML 文件，例如 `main.yml`。
+
+4. **YAML 配置示例**:
+
+   ```yaml
+   name: MiHoYo Sign-In Script
+
+   on:
+     schedule:
+       - cron: '20 0 * * *' # 每天UTC 0:20, 对应北京时间8:20
+     workflow_dispatch: # 允许手动触发
+
+   jobs:
+     build-and-run:
+       runs-on: ubuntu-latest
+
+       steps:
+       - name: Checkout repository
+         uses: actions/checkout@v3
+
+       - name: Set up Node.js
+         uses: actions/setup-node@v3
+         with:
+           node-version: '18'
+
+       - name: Install dependencies
+         run: npm install
+
+       - name: Run main.js
+         env:
+           GENSHIN_COOKIES: ${{ secrets.GENSHIN_COOKIES }}
+         run: node main.js
+   ```
+
+## 本地部署
+
+1. **安装 Node.js**:
+   - 确保你已安装 Node.js 版本大于 14.x。
+   
+2. **Clone项目**:
+   ```sh
+   git clone https://github.com/GildedFlames/MYS_Game_Singin.git
+   ```
+   
+3. **安装依赖**:
+   ```sh
+   npm install
+   ```
+
+4. **在 main.js getCookieConfig 方法中设置Cookie信息**:
+     ```js
+     const genshinCookies = 'cookie1,cookie2'
+     ```
+
+5. **运行脚本**:
+   ```sh
+   node main.js
+   ```
+
+## 更新日志
+
+- **2025-02-08**: 初始发布，支持原神和崩铁多账号签到功能。
---
+++ main.js
@@ -0,0 +1,180 @@
+const axios = require('axios');
+const md5 = require('md5');
+const uuid = require('uuid');
+
+let ROLE = {
+  Genshin: {
+    game_biz: '',
+    region: '',
+    game_uid: '',
+    nickname: '',
+    level: -1,
+    is_chosen: false,
+    region_name: '',
+    is_official: false
+  },
+  StarRail: {
+    game_biz: '',
+    region: '',
+    game_uid: '',
+    nickname: '',
+    level: -1,
+    is_chosen: false,
+    region_name: '',
+    is_official: false
+  }
+}
+
+const WEB_HOST = 'api-takumi.mihoyo.com'
+const APP_VERSION = '2.81.1'
+
+const COMMON__HEADERS = {
+  "DS": '',
+  "Cookie": '',
+  "Host": WEB_HOST,
+  "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 18_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/${APP_VERSION}`,
+  "x-rpc-app_version": APP_VERSION,
+  "x-rpc-client_type": 5,
+  "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+  "Accept": "application/json, text/plain, */*",
+}
+let ROLE_HEADERS = {
+  "Referer": 'https://webstatic.mihoyo.com/',
+  "x-rpc-device_id": uuid.v4(),
+  "Origin": "https://webstatic.mihoyo.com",
+  "x-rpc-challenge": 'null',
+  "Accept": "application/json, text/plain, */*",
+  "Accept-Encoding": "gzip, deflate, br",
+};
+let SIGN_HEADERS = {
+  "Referer": "https://act.mihoyo.com/",
+  "x-rpc-device_model": "iPhone14,4",
+  "x-rpc-device_id": uuid.v4(),
+  "x-rpc-platform": 1,
+  "x-rpc-device_name": "iPhone",
+  "Origin": "https://act.mihoyo.com",
+  "Sec-Fetch-Site": "same-site",
+  "Connection": "keep-alive",
+  "Content-Type": "application/json;charset=utf-8",
+}
+
+const $axios = axios.create({})
+
+const getCookieConfig = async () => {
+  // 检查是否有通过环境变量传入的数据, 本地部署需要修改
+  const genshinCookies = process.env.GENSHIN_COOKIES;
+
+  if (!genshinCookies) {
+    throw new Error("Missing required environment variables.");
+  }
+
+  try {
+    const genshinCookieArr = genshinCookies ? genshinCookies.split(',') : []
+    // 米家游戏可通用Cookie
+    return { Genshin: genshinCookieArr, StarRail: genshinCookieArr }
+  } catch (error) {
+    console.error("Failed to parse environment variable data as JSON:", error.message);
+    throw new Error("Failed to parse environment variable data as JSON.");
+  }
+}
+
+const randomSleep = (min, max) => {
+  const delay = Math.floor(Math.random() * (max - min + 1)) + min
+  console.log(`Sleeping for ${delay} seconds...`);
+  return new Promise((resolve) => setTimeout(resolve, delay * 1000))
+}
+
+async function getDS () {
+    const s = "yUZ3s0Sna1IrSNfk29Vo6vRapdOyqyhB";
+    const t = Math.floor(Date.now() / 1e3);
+    const r = Math.random().toString(36).slice(-6);
+    const c = `salt=${s}&t=${t}&r=${r}`;
+    return `${t},${r},${md5(c)}`;
+}
+
+const getHeaders = async (Cookie, whichHeader) => {
+  return { ...COMMON__HEADERS, ...whichHeader, Cookie, DS: await getDS() }
+}
+
+const getRole = async (cookie, gameKey) => {
+  const GAME_BIZ = { Genshin: 'hk4e_cn', StarRail: 'hkrpg_cn' }
+  const headers = await getHeaders(cookie, ROLE_HEADERS)
+  const res = await $axios.request({
+    method: 'GET',
+    headers,
+    url: `https://${WEB_HOST}/binding/api/getUserGameRolesByCookie?game_biz=${GAME_BIZ[gameKey]}`
+  }).catch(err => {
+      console.error('登录错误\n' + err);
+      return 0;
+  });
+  if (res.data['retcode'] !== 0) {
+    console.info('帐号未登录,请检查cookie');
+    return 0;
+  }
+  if ((res?.data?.message === 'OK') && res.data.data.list[0]) {
+    ROLE[gameKey] = res.data.data.list[0]
+    console.log(`[${gameKey}] 登陆成功 <${ROLE[gameKey].nickname}(${ROLE[gameKey].game_uid})>: `, JSON.stringify(res.data))
+  } else {
+    ROLE[gameKey] = {
+      game_biz: '',
+      region: '',
+      game_uid: '',
+      nickname: '',
+      level: -1,
+      is_chosen: false,
+      region_name: '',
+      is_official: false
+    }
+    console.log(`[${gameKey}] 登陆失败 <未查询到角色>: `, JSON.stringify(res.data))
+  }
+}
+
+async function Sign_In(cookie, gameKey) {
+  const ACT_ID = { Genshin: 'e202311201442471', StarRail: 'e202304121516551' }
+  const REGION = { Genshin: 'cn_gf01', StarRail: 'prod_gf_cn' }
+  const SIGNGAME = { Genshin: 'hk4e', StarRail: 'hkrpg' }
+  
+  const headers = await getHeaders(cookie, { ...SIGN_HEADERS, 'x-rpc-signgame': SIGNGAME[gameKey] })
+  const data = {
+    act_id: ACT_ID[gameKey],
+    region: REGION[gameKey],
+    uid: ROLE[gameKey].game_uid,
+    lang: 'zh-cn'
+  }
+  const res = await $axios.request({
+    method: 'POST',
+    headers,
+    data,
+    url: `https://${WEB_HOST}/event/luna/${SIGNGAME[gameKey]}/sign`
+  }).catch(err => {
+    console.error('签到错误\n' + err)
+  })
+  console.log(`<${ROLE[gameKey].nickname}(${ROLE[gameKey].game_uid})>签到${res?.data?.message === 'OK' ? '成功' : '失败'}: `, JSON.stringify(res.data))
+}
+
+const doGameSign = async (gameKey) => {
+  const CONF = await getCookieConfig()
+  const cookieList = CONF[gameKey]
+  if (cookieList.length) {
+    console.info(`[${gameKey}] 签到开始, 共获取 ${cookieList.length} 位用户\n`)
+    for (const cookIndex in cookieList) {
+      const cook = cookieList[cookIndex]
+      if (cook) {
+        console.log(`第 ${Number(cookIndex) + 1} 位用户开始签到...`)
+        await getRole(cook, gameKey)
+        if (ROLE[gameKey]?.game_uid) {
+          await Sign_In(cook, gameKey)
+        }
+        await randomSleep(3, 9)
+      }
+    }
+    console.info(`[${gameKey}] 签到 结束\n`)
+  }
+}
+
+async function main() {
+  await doGameSign('Genshin')
+  await doGameSign('StarRail')
+}
+
+main().then()
